baaf01210b7da1809cef7484e20feb562995b495
Kurama <60360593+Kurama250@users.noreply.github.com>
2024-12-14 21:32:58 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,13 +4,22 @@
 
 apt update && apt upgrade -y
 apt install npm nodejs git -y
+
 if ! command -v node &> /dev/null; then
-    curl -fsSL https://deb.nodesource.com/setup_20.x | bash - &&\
-    apt-get install -y nodejs -y
+    echo "Node.js is not installed. Installing..."
+    curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+    apt-get install -y nodejs
 else
     echo "Node.js is already installed. Skipping installation."
 fi
+
 git clone https://github.com/Kurama250/Alert_directory_V2.git
 cd Alert_directory_V2/
 npm install fs axios tail ua-parser-js useragent systeminformation
-npm install pm2 -g
+
+if ! command -v pm2 &> /dev/null; then
+    echo "PM2 is not installed. Installing..."
+    npm install pm2 -g
+else
+    echo "PM2 is already installed. Skipping installation."
+fi
